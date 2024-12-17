a6243936d12dff9a9b89f28a97a5f09e4c9f6fa3
Emre <emre.kaya.2000.offical@gmail.com>
2024-12-17 13:57:58 +0300

    tasks ve categori tablosu eklendi foreign bağlantıları oluşturuldu

---
+++ database/migrations/2024_12_17_104142_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title')->nullable();
+            $table->string('content')->nullable();
+            $table->tinyInteger('status')->default(1)->comment('0-yapılmadı 1-yapılıyor -2 ertelendi 3-iptal oldu');
+            $table->dateTime('deadLine')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
